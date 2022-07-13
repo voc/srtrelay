789d1942fbd2318540fd1f64e688058b21652ccb
Anton Schubert <ischluff@mailbox.org>
2022-07-13 11:44:51 +0200

    ci: set install prefix

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -2,6 +2,6 @@
 set -e
 sudo apt-get install -y tclsh pkg-config cmake libssl-dev build-essential ffmpeg ninja-build
 git clone --depth 1 --branch v1.5.0 https://github.com/Haivision/srt.git libsrt
-cmake -S libsrt -B libsrt-build -G Ninja
+cmake -DCMAKE_INSTALL_PREFIX=/usr -S libsrt -B libsrt-build -G Ninja
 ninja -C libsrt-build
 ninja -C libsrt-build install
