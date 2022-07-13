93f54e4ba0a64b6304303296d7edfdc6223718dc
Anton Schubert <ischluff@mailbox.org>
2022-07-13 11:47:41 +0200

    ci: switch back to make

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
-sudo apt-get install -y tclsh pkg-config cmake libssl-dev build-essential ffmpeg ninja-build
+sudo apt-get install -y tclsh pkg-config cmake libssl-dev build-essential ffmpeg
 git clone --depth 1 --branch v1.5.0 https://github.com/Haivision/srt.git libsrt
-cmake -DCMAKE_INSTALL_PREFIX=/usr -S libsrt -B libsrt-build -G Ninja
-ninja -C libsrt-build
-ninja -C libsrt-build install
+cd libsrt
+./configure
+make -j
+sudo make install
