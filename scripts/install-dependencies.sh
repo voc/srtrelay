6cd5732eed869f305fab53bb087f35208d08d975
Anton Schubert <ischluff@mailbox.org>
2022-07-13 11:40:28 +0200

    update to libsrt 1.5.0, update build

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 set -e
-sudo apt-get install -y tclsh pkg-config cmake libssl-dev build-essential ffmpeg
-git clone --depth 1 --branch v1.4.2 https://github.com/Haivision/srt.git libsrt
-cd libsrt
-./configure
-make -j
-sudo make install
+sudo apt-get install -y tclsh pkg-config cmake libssl-dev build-essential ffmpeg ninja-build
+git clone --depth 1 --branch v1.5.0 https://github.com/Haivision/srt.git libsrt
+cmake -S libsrt -B libsrt-build -G Ninja
+ninja -C libsrt-build
+ninja -C libsrt-build install
