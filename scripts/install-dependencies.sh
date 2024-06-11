de3ad9e8fae342e025a9857fbd3a49f3de40dcd4
Anton Schubert <ischluff@mailbox.org>
2024-06-11 12:23:58 +0200

    ci: try to fix dependency install

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
-sudo apt-get install -y tclsh pkg-config cmake libssl-dev build-essential ffmpeg
-git clone --depth 1 --branch v1.5.0 https://github.com/Haivision/srt.git libsrt
+sudo apt update
+sudo apt install -y tclsh pkg-config cmake libssl-dev build-essential ffmpeg
+git clone --depth 1 --branch v1.5.3 https://github.com/Haivision/srt.git libsrt
 cd libsrt
 ./configure
 make -j
