3f7a858b876399fa24081e4488466a86e7756595
Paulson McIntyre <paul@gpmidi.net>
2024-11-28 10:03:42 -0500

    Using new format for get stats

--- srt/server.go
+++ srt/server.go
@@ -332,8 +332,8 @@ func (s *ServerImpl) registerForStats(ctx context.Context, conn *srtConn) {
 func (s *ServerImpl) GetStatistics() []*relay.StreamStatistics {
 	streams := s.relay.GetStatistics()
 	for _, st := range streams {
-		//stream.URL = fmt.Sprintf("srt://%s?streamid=#!::m=request,r=%s", s.config.PublicAddress, stream.Name)
-		st.URL = fmt.Sprintf("srt://%s?streamid=play/%s", s.config.PublicAddress, st.Name)
+		st.URL = fmt.Sprintf("srt://%s?streamid=#!::m=request,r=%s", s.config.PublicAddress, st.Name) // New format
+		//st.URL = fmt.Sprintf("srt://%s?streamid=play/%s", s.config.PublicAddress, st.Name) // Old format
 	}
 	return streams
 }
