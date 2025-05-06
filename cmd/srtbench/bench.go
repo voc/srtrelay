3bef2ef0cf64a489879b019271632219fa43d629
Anton Schubert <ischluff@mailbox.org>
2025-05-06 02:34:40 +0200

    add srt bench tool

---
+++ cmd/srtbench/bench.go
@@ -0,0 +1,192 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"sync/atomic"
+	"syscall"
+	"time"
+
+	gosrt "github.com/datarhei/gosrt"
+)
+
+func main() {
+	addr := flag.String("addr", "localhost:1337", "address to connect to")
+	streams := flag.Int("streams", 1, "number of streams")
+	ratio := flag.Int("ratio", 5, "number of subscribers per publisher")
+	flag.Parse()
+	var pubs []*pub
+	var subs []*sub
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	ctx, _ = signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	t := &test{
+		addr:    *addr,
+		errChan: make(chan error, 1),
+		log:     gosrt.NewLogger([]string{"connection:error"}),
+	}
+	for i := range *streams {
+		pub, err := t.runPublisher(ctx, fmt.Sprintf("publish/%d", i))
+		if err != nil {
+			log.Fatal("publisher:", err)
+		}
+		pubs = append(pubs, pub)
+		for range *ratio {
+			sub, err := t.runSubscriber(ctx, fmt.Sprintf("play/%d", i))
+			if err != nil {
+				log.Fatal("subscriber:", err)
+			}
+			subs = append(subs, sub)
+		}
+	}
+	lastWrite := int64(0)
+	lastRead := int64(0)
+	for {
+		select {
+		case <-ctx.Done():
+		case err := <-t.errChan:
+			cancel()
+			log.Println("error:", err)
+		case log := <-t.log.Listen():
+			fmt.Printf("[%s] %s %s:%d\n", log.Topic, log.Message, log.File, log.Line)
+			continue
+		case <-time.After(time.Second):
+			writeTotal := int64(0)
+			// var stats gosrt.Statistics
+			for _, pub := range pubs {
+				writeTotal += pub.written.Load()
+				// pub.conn.Stats(&stats)
+			}
+			readTotal := int64(0)
+			for _, sub := range subs {
+				readTotal += sub.read.Load()
+			}
+			log.Printf("write: %.3fMbit/s, read: %.3fMbit/s", float64(writeTotal-lastWrite)/1e6*8, float64(readTotal-lastRead)/1e6*8)
+			lastWrite = writeTotal
+			lastRead = readTotal
+			continue
+		}
+		break
+	}
+	t.done.Wait()
+	log.Println("done")
+}
+
+type test struct {
+	addr    string
+	done    sync.WaitGroup
+	errChan chan error
+	log     gosrt.Logger
+}
+
+func (t *test) error(err error) {
+	select {
+	case t.errChan <- err:
+	default:
+	}
+}
+
+type pub struct {
+	conn    gosrt.Conn
+	written atomic.Int64
+}
+
+type sub struct {
+	conn gosrt.Conn
+	read atomic.Int64
+}
+
+// run single publisher sending to relay
+func (t *test) runPublisher(ctx context.Context, streamid string) (*pub, error) {
+	conf := gosrt.DefaultConfig()
+	conf.StreamId = streamid
+	conf.Latency = 50 * time.Millisecond
+	conn, err := gosrt.Dial("srt", t.addr, conf)
+	if err != nil {
+		return nil, err
+		// handle error
+	}
+	buffer := make([]byte, 1316)
+	pub := &pub{
+		conn: conn,
+	}
+	t.done.Add(2)
+	context.AfterFunc(ctx, func() {
+		defer t.done.Done()
+		conn.Close()
+	})
+
+	go func() {
+		defer t.done.Done()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-time.After(500 * time.Microsecond):
+			}
+			conn.SetWriteDeadline(time.Now().Add(300 * time.Millisecond))
+			n, err := conn.Write(buffer)
+			if err != nil {
+				if ctx.Err() != nil {
+					return
+				}
+				// handle error
+				t.error(fmt.Errorf("write failed: %w", err))
+				break
+			}
+			// handle received data
+			pub.written.Add(int64(n))
+		}
+	}()
+	return pub, nil
+}
+
+func (t *test) runSubscriber(ctx context.Context, streamid string) (*sub, error) {
+	conf := gosrt.DefaultConfig()
+	conf.Latency = 50 * time.Millisecond
+	conf.StreamId = streamid
+	conf.PeerIdleTimeout = time.Second * 10
+	conf.ConnectionTimeout = time.Second * 10
+	conf.Logger = t.log
+	conn, err := gosrt.Dial("srt", t.addr, conf)
+	if err != nil {
+		return nil, err
+		// handle error
+	}
+	buffer := make([]byte, 2048)
+	sub := &sub{
+		conn: conn,
+	}
+	t.done.Add(2)
+	context.AfterFunc(ctx, func() {
+		defer t.done.Done()
+		conn.Close()
+	})
+
+	go func() {
+		defer t.done.Done()
+		for {
+			if ctx.Err() != nil {
+				return
+			}
+			conn.SetReadDeadline(time.Now().Add(300 * time.Millisecond))
+			n, err := conn.Read(buffer)
+			if err != nil {
+				if ctx.Err() != nil {
+					return
+				}
+				// handle error
+				t.error(fmt.Errorf("read failed: %w", err))
+				break
+			}
+			sub.read.Add(int64(n))
+		}
+	}()
+	return sub, nil
+}
--- go.mod
+++ go.mod
@@ -12,14 +12,17 @@ require (
 )
 
 require (
+	github.com/benburkert/openpgp v0.0.0-20160410205803-c2471f86866c // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/datarhei/gosrt v0.9.0 // indirect
 	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/mattn/go-pointer v0.0.1 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.62.0 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
+	golang.org/x/crypto v0.33.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	google.golang.org/protobuf v1.36.5 // indirect
 )
